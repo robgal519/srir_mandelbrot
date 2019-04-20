108b2c919faf59246265b9d392f26a2ad8044e96
Robert Gałat <robgal519@gmail.com>
2019-04-20 17:03:27 +0200

    problemy z komunikacją

--- mpi_mandelbrot/main.cpp
+++ mpi_mandelbrot/main.cpp
@@ -8,10 +8,14 @@
 #include <thread>
 #include <cmath>
 #include <vector>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <mpi.h>
 
 #include "colors.hpp"
+
 
 
 int procid, numprocs;
@@ -34,23 +38,71 @@ unsigned int inMandelbrot(double x, double y) {
     return iteration;
 }
 
+struct Request {
+    bool connectionOk;
+    int windowWidth;
+    int windowHeight;
+    double leftTopX;
+    double leftTopY;
+    double rightBottomX;
+    double rightBottomY;
+};
 
 int main(int argc, char *argv[]) {
     int err = MPI_Init(&argc, &argv);
 
     err = MPI_Comm_rank(MPI_COMM_WORLD, &procid);
     err = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-
+    std::string req = "/tmp/.req";
+    std::string resp = "/tmp/.resp";
+    int requestPipe;
+    int responsePipe;
 //////////////////////////////////
-    int width = 1700;
-    int hight = 600;
-// whole picture is in rectangle between {-2,1} and {1,-1}
-    std::pair<double, double> start = {-2, 1};
-    std::pair<double, double> end = {1, -1};
+if(procid == 0){
+    mkfifo(req.c_str(), 0777);
+    mkfifo(resp.c_str(), 0777);
+
+    std::cout << "T: Opening req R" << std::endl;
+    int requestPipe = open(req.c_str(), O_RDONLY);
+    if(requestPipe == -1){
+        throw -1;
+    }
+    std::cout << "T: Opened req R" << std::endl;
+    std::cout << "T: Opening resp W" << std::endl;
+    int responsePipe = open(resp.c_str(), O_WRONLY);
+    if(responsePipe == -1) {
+        throw -1;
+    }
+    std::cout << "T: Opened resp W" << std::endl;
+
+}
+bool condition = true;
+while(condition) {
+    Request request;
+    int status;
+    if (procid == 0) {
+        std::cout<< "0 read request"<<std::endl;
+        status = read(requestPipe, &request, sizeof(Request));
+        if(status == -1){
+            throw -1;
+        }
+        std::cout<<"0 request readed"<<std::endl;   
+    }
+    MPI_Bcast(&request, sizeof(Request),MPI_CHAR,0,MPI_COMM_WORLD);
+    std::cout<<procid << " received broadcast\n";
+    int width = request.windowWidth;
+    int hight = request.windowHeight;
+    condition = request.connectionOk;
+    if(!condition)
+        continue;
+    std::cout<< procid<< " "<< width << " " << hight<<std::endl;
+    // whole picture is in rectangle between {-2,1} and {1,-1}
+    std::pair<double, double> start = {request.leftTopX, request.leftTopY};
+    std::pair<double, double> end = {request.rightBottomX, request.rightBottomY};
     std::vector<char> result;
     if (procid != 0) {
 
-        for (int y = procid-1; y < hight; y+= (numprocs-1)) {
+        for (int y = procid - 1; y < hight; y += (numprocs - 1)) {
             //std::vector<RGB> line(width);
             char line[width * 3];
             for (int x = 0; x < width; x++) {
@@ -67,20 +119,23 @@ int main(int argc, char *argv[]) {
     } else {
         for (int y = 0; y < hight; y++) {
             char line[width * 3];
-                MPI_Status status;
-                MPI_Recv(&line, width * 3, MPI_CHAR, y%(numprocs-1) +1, 0, MPI_COMM_WORLD, &status);
-                result.insert(std::end(result), line, line + width * 3);
+            MPI_Status status;
+            MPI_Recv(&line, width * 3, MPI_CHAR, y % (numprocs - 1) + 1, 0, MPI_COMM_WORLD, &status);
+            result.insert(std::end(result), line, line + width * 3);
 
         }
-        std::ofstream plot("data.ppm");
+        //std::ofstream plot("data.ppm");
 
-        plot << "P6" << std::endl;
-        plot << width << " " << hight << std::endl;
-        plot << "255" << std::endl;
+        write(responsePipe,result.data(),result.size() );
 
-        for (const auto &a:result)
-            plot << a;
-        plot.close();
+        //plot << "P6" << std::endl;
+        //plot << width << " " << hight << std::endl;
+        //plot << "255" << std::endl;
+
+        //for (const auto &a:result)
+        //    plot << a;
+        //plot.close();
     }
+}
     MPI_Finalize();
 }
